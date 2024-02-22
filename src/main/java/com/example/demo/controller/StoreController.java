ec735adc96e130ce9f16def8bd4b014cb5cc3b0f
Arnaud <arnaud.chane.dev@gmail.com>
2024-02-22 16:33:37 +0100

    WIP - Create StoreController but can't display all data addresses on browser

--- src/main/java/com/example/demo/controller/StoreController.java
+++ src/main/java/com/example/demo/controller/StoreController.java
@@ -1,4 +1,21 @@
 package com.example.demo.controller;
 
+import com.example.demo.model.Address;
+import com.example.demo.repository.MySqlRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
 public class StoreController {
+
+    @Autowired
+    MySqlRepository mySqlRepository;
+
+    @GetMapping("/get-all-addresses")
+    public List<Address> getAllAddresses(){
+        return mySqlRepository.findAll();
+    }
 }
