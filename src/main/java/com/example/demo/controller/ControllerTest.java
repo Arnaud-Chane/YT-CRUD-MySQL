27d106bf3f0c21a5cbedca5f2e35e4e9ade32b9c
Arnaud <arnaud.chane.dev@gmail.com>
2024-02-22 12:10:49 +0100

    Route person return json person data

--- src/main/java/com/example/demo/controller/ControllerTest.java
+++ src/main/java/com/example/demo/controller/ControllerTest.java
@@ -1,5 +1,6 @@
 package com.example.demo.controller;
 
+import com.example.demo.model.Person;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -24,5 +25,13 @@ public class ControllerTest {
     @GetMapping("/test3")
     public String getTest3(){
         return "test 3";
+    }
+
+    @GetMapping("/person")
+    public Person getPerson(){
+        Person p = new Person();
+        p.setName("Arnaud");
+        p.setAge(30);
+        return p;
     }
 }
