3db2fac074b8617182c9221c15249385a10f5555
Arnaud-Chane <arnaud.chane.dev@gmail.com>
2024-02-24 17:37:34 +0100

    Updating id,pw to DB in app.prop & updating address.sql

--- address.sql
+++ address.sql
@@ -1,16 +1,20 @@
+--@block
 CREATE TABLE Address (
-    id INT AUTO_INCREMENT,
-    number INT,
-    street VARCHAR(255),
-    postcode VARCHAR(255),
-    PRIMARY KEY (id)
+  id INT AUTO_INCREMENT,
+  number INT,
+  street VARCHAR(255),
+  postcode VARCHAR(255),
+  PRIMARY KEY (id)
 );
-
-INSERT INTO Address (number, street, postcode) VALUES
-(123, 'Baker Street', 'NW1 6XE'),
-(456, 'Fleet Street', 'EC4A 2BH'),
-(789, 'King’s Road', 'SW3 5XP'),
-(101, 'Oxford Street', 'W1D 2LA'),
-(112, 'Brick Lane', 'E1 6RL');
-
---DROP TABLE address;
+--@block
+INSERT INTO Address (number, street, postcode)
+VALUES (123, 'Baker Street', 'NW1 6XE'),
+  (456, 'Fleet Street', 'EC4A 2BH'),
+  (789, 'King’s Road', 'SW3 5XP'),
+  (101, 'Oxford Street', 'W1D 2LA'),
+  (112, 'Brick Lane', 'E1 6RL');
+--@block
+DROP TABLE address;
+--@block
+SELECT *
+FROM Address;
