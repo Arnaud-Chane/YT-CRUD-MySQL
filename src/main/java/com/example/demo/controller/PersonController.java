f209d0229eb9ddfb25d4b0f3472d20b58375ee5d
Arnaud-Chane <arnaud.chane.dev@gmail.com>
2024-02-29 09:34:50 +0100

    Adding a /endpoint GetMapping in PersonController

--- src/main/java/com/example/demo/controller/PersonController.java
+++ src/main/java/com/example/demo/controller/PersonController.java
@@ -8,30 +8,35 @@ import org.springframework.web.bind.annotation.RestController;
 public class PersonController {
 
     @GetMapping("/")
-    public String getHello(){
+    public String getHello() {
         return "Hello test";
     }
 
-
     @GetMapping("/test")
-    public String getTest(){
+    public String getTest() {
         return "test 123456";
     }
 
     @GetMapping("/test2")
-    public String getTest2(){
+    public String getTest2() {
         return "test 2";
     }
+
     @GetMapping("/test3")
-    public String getTest3(){
+    public String getTest3() {
         return "test 3";
     }
 
     @GetMapping("/person")
-    public Person getPerson(){
+    public Person getPerson() {
         Person p = new Person();
         p.setName("Arnaud");
         p.setAge(30);
         return p;
+    }
+
+    @GetMapping("/endpoint")
+    public String endpoint() {
+        return "endpoint";
     }
 }
