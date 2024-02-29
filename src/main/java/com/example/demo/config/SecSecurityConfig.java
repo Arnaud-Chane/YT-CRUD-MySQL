2d9c5ae0489e291132cca4d745126e1fc478e6fe
Arnaud-Chane <arnaud.chane.dev@gmail.com>
2024-02-29 09:33:18 +0100

    trying baeldung tutorial but not working & renaming ControllerTest by PersonController

--- src/main/java/com/example/demo/config/SecSecurityConfig.java
+++ src/main/java/com/example/demo/config/SecSecurityConfig.java
@@ -1,5 +1,6 @@
 package com.example.demo.config;
 
+import jakarta.servlet.ServletContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -10,6 +11,11 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.access.expression.WebExpressionAuthorizationManager;
+import org.springframework.web.WebApplicationInitializer;
+import org.springframework.web.context.ContextLoaderListener;
+import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
+import org.springframework.web.filter.DelegatingFilterProxy;
 
 @Configuration
 @EnableWebSecurity
@@ -39,6 +45,29 @@ public class SecSecurityConfig {
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        // http builder configurations for authorize requests and form login (see below)
+        http
+            .authorizeHttpRequests(authorize -> authorize
+            .requestMatchers("/login/**", "/signup", "/about").permitAll()
+            .requestMatchers("/admin/**").hasRole("ADMIN")
+            .requestMatchers("/db/**").access(new WebExpressionAuthorizationManager("hasRole('ADMIN')"))
+            .anyRequest().denyAll()
+        );
+        return http.build();
     }
+
+    public class AppInitializer implements WebApplicationInitializer {
+
+        @Override
+        public void onStartup(ServletContext sc) {
+
+            AnnotationConfigWebApplicationContext root = new AnnotationConfigWebApplicationContext();
+            root.register(SecSecurityConfig.class);
+
+            sc.addListener(new ContextLoaderListener(root));
+
+            sc.addFilter("securityFilter", new DelegatingFilterProxy("springSecurityFilterChain"))
+                    .addMappingForUrlPatterns(null, false, "/*");
+        }
+    }
+
 }
--- src/main/java/com/example/demo/controller/PersonController.java
+++ src/main/java/com/example/demo/controller/PersonController.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class ControllerTest {
+public class PersonController {
 
     @GetMapping("/")
     public String getHello(){
